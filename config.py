dadaae9b8c770118d961ad8f2321ec3e55de4dc9
CHARITY LEFOSA <125449623+charitylefosa@users.noreply.github.com>
2024-06-05 19:46:08 -0400

    Update config.py - Conflict in file resolved

--- config.py
+++ config.py
@@ -10,13 +10,8 @@ class Config:
     
     # Disable SQLAlchemy's modification tracking feature to save memory
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-<<<<<<< HEAD
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
 
-=======
-    
     # Secret key for JWT authentication
     # Uses an environment variable if available, else defaults to a hardcoded secret
     # Change this in production to a secure random key
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
->>>>>>> 9381b34025beebf4c5d19858c3b35927545cffdf

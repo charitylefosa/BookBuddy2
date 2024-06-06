44918e04c308295e0ff36187ece4ca2c11a0753a
CHARITY LEFOSA <125449623+charitylefosa@users.noreply.github.com>
2024-06-05 22:06:57 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
 
 class Config:
-    # Secret key for session management and other security-related features
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Secret key for session management
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-not-guess'
     
     # Database URI for SQLAlchemy to connect to the database
-    # Uses an environment variable if available, otherwise defaults to a SQLite database file
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///bookbuddy.db'
     
-    # Disable SQLAlchemy's modification tracking feature to save memory
+    # Disable SQLAlchemy's modification tracking feature - save memory
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Secret key for JWT authentication
-    # Uses an environment variable if available, else defaults to a hardcoded secret
-    # Change this in production to a secure random key
+    # Secret key for JWT(Json Web Token) authentication
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
